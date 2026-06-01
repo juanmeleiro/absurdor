b5003ff85f32553d4cfbbd2af8d5bec03219d064
Juan F. Meleiro <juan@juanmeleiro.mat.br>
2026-06-01 20:26:29 -0300

    Weekly report

--- lib/report/Banner.lua
+++ lib/report/Banner.lua
@@ -1,61 +1,62 @@
 local Module = require "lib/report/Module"
 local pprint = require("pprint").pprint
 
-local Banner = Module:new({
-	height = 0,
-	slope = 1,
-	max_height = 0,
-	max_slope = 1,
-	weekly_pushes = {},
-	fell_last_week = false
-})
+function Banner(wow)
+	local b = Module:new({
+		height = 0,
+		slope = 1,
+		max_height = 0,
+		max_slope = 1,
+		weekly_pushes = {},
+		fell_last_week = false,
+		wow = wow
+	})
 
-function maybe_start_week(data, w)
-	if data.weekly_pushes[w] == nil then -- Starting a new week. Need to initialize it:
-		local p = previous_week(w)
-		if data.weekly_pushes[p] then -- Last week exists. Check conditions for falling:
-			if data.weekly_pushes[p] >= data.slope then -- Last week was successful
-				data.slope = data.slope + 1
-				data.max_slope = math.max(data.max_slope, data.slope)
+	function maybe_start_week(data, w)
+		if data.weekly_pushes[w] == nil then -- Starting a new week. Need to initialize it:
+			local p = previous_week(w)
+			if data.weekly_pushes[p] then -- Last week exists. Check conditions for falling:
+				if data.weekly_pushes[p] >= data.slope then -- Last week was successful
+					data.slope = data.slope + 1
+					data.max_slope = math.max(data.max_slope, data.slope)
+					data.weekly_pushes[w] = 0
+					data.fell_last_week = false
+				else -- Last week failed.
+					data.weekly_pushes[w] = 1 -- Counting the current push.
+					data.height = 1
+					data.slope = 1
+					data.fell_last_week = true
+				end
+			else -- First week ever.
 				data.weekly_pushes[w] = 0
-				data.fell_last_week = false
-			else -- Last week failed.
-				data.weekly_pushes[w] = 1 -- Counting the current push.
-				data.height = 1
-				data.slope = 1
-				data.fell_last_week = true
 			end
-		else -- First week ever.
-			data.weekly_pushes[w] = 0
 		end
 	end
-end
-
-Banner:add_processor("push", function(self, e)
-	local w = unix_to_week(e.when)
-	maybe_start_week(self, w)
-
-	self.height = self.height + 1
-	self.weekly_pushes[w] = self.weekly_pushes[w] + 1
-
-	-- Update the known maxima
-	self.max_height = math.max(self.height, self.max_height)
-	self.max_slope = math.max(self.slope, self.max_slope)
-end)
-
-Banner:add_processor("report", function(self, e)
-	local w = unix_to_week(e.when)
-	maybe_start_week(self, w)
-
-	self.height = e.height
-	self.slope = e.slope
-end)
-
-function Banner:render(out)
-	maybe_start_week(self, unix_to_week(os.time()))
-	local templates = 
-	{
-		regular = [[
+	
+	b:add_processor("push", function(self, e)
+		local w = unix_to_week(e.when)
+		maybe_start_week(self, w)
+	
+		self.height = self.height + 1
+		self.weekly_pushes[w] = self.weekly_pushes[w] + 1
+	
+		-- Update the known maxima
+		self.max_height = math.max(self.height, self.max_height)
+		self.max_slope = math.max(self.slope, self.max_slope)
+	end)
+	
+	b:add_processor("report", function(self, e)
+		local w = unix_to_week(e.when)
+		maybe_start_week(self, w)
+	
+		self.height = e.height
+		self.slope = e.slope
+	end)
+	
+	function b:render(out)
+		maybe_start_week(self, unix_to_week(os.time()))
+		local templates = {
+			regular = [[
 
             #####     _/
            #######   /
@@ -73,7 +74,7 @@ function Banner:render(out)
    /
 
 ]],
-		fall = [[
+			fall = [[
                        / 
   argh!    /   __,   /   
         O.--../      /   
@@ -89,24 +90,50 @@ function Banner:render(out)
      /                  
    /                    
 
-]]
-	}
+]],
+			woosh = [[
+              `   /#\ _/
+           `     /###/
+        .     . //\#//
+            ,   // _/
+   huh         /  /
+        O        /
+      / / _/   /                   The Boulder is FLYING!
+     /_ /_    /                      It is at %d
+      /_/\.  /                         Its Slope is %d
+      |  // _/
+      \  ‘\/                       It has been at %d
+      |_ /                          Moving as fas as %d
+    _/
+   /
 
-	if self.fell_last_week then
-		out:write(string.format(templates.fall, self.height, self.slope, self.max_height, self.max_slope))
-	else
-		out:write(string.format(templates.regular, self.height, self.slope, self.max_height, self.max_slope))
+
+
+]]
+		}
+	
+		if self.fell_last_week then
+			out:write(string.format(templates.fall, self.height, self.slope, self.max_height, self.max_slope))
+		elseif self.wow then
+			out:write(string.format(templates.woosh, self.height, self.slope, self.max_height, self.max_slope))
+		else
+			out:write(string.format(templates.regular, self.height, self.slope, self.max_height, self.max_slope))
+		end
 	end
+	
+	b:add_field("height", function(self)
+		maybe_start_week(self, unix_to_week(os.time()))
+		return self.height
+	end)
+	
+	b:add_field("slope", function(self)
+		maybe_start_week(self, unix_to_week(os.time()))
+		return self.slope
+	end)
+	
+	return b
+
 end
 
-Banner:add_field("height", function(self)
-	maybe_start_week(self, unix_to_week(os.time()))
-	return self.height
-end)
-
-Banner:add_field("slope", function(self)
-	maybe_start_week(self, unix_to_week(os.time()))
-	return self.slope
-end)
-
 return Banner
+
