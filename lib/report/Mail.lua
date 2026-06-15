1f93d24f0c84a58d480dc8a303e2ebae5ec0c988
Juan F. Meleiro <juan@juanmeleiro.mat.br>
2026-06-15 11:51:26 -0300

    Weekly report

--- lib/report/Mail.lua
+++ lib/report/Mail.lua
@@ -10,7 +10,7 @@ From: juan <juan@juanmeleiro.mat.br>
 Subject: [Absurdor] The State of the Absurd (%s)
 
 ]]
-	out:write(string.format(template, os.date("%a,%e %b %Y %H:%M:%H %z"), os.date("%Y-%U")))
+	out:write(string.format(template, os.date("%a,%e %b %Y %H:%M:%H %z"), os.date("%Yw%U")))
 end
 
 return Mail
